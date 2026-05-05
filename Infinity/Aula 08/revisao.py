25c8da7a2e3b13f0c53092a49027503c4a2ad1cc
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-05-05 20:13:03 -0300

    Aula 08

---
+++ Infinity/Aula 08/revisao.py
@@ -0,0 +1,10 @@
+# alunos = {
+#     "Ana": 8.5,
+#     "Bruno": 6.0,
+#     "Rafael": 7.2,
+#     "Pedro": 5.5
+# }
+
+# for nome, nota in alunos.items():
+#     if nota >= 7:
+#         print(nome, nota)
